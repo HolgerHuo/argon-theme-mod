006c5768a3f7157aececabbfe927520e85c69666
Holger Huo <holgerhuo@outlook.com>
2022-12-26 20:36:02 +0800

    update: umami link

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function add_headers() {
     </style>
     <?php
 		if ( !is_user_logged_in() ) { ?>
-			<script async defer data-website-id="0a96dd1e-75a2-49e4-90ec-3a575b3eb12c" src="https://stats.dragon-fly.club/umami.js"></script>
+			<script async defer data-website-id="0a96dd1e-75a2-49e4-90ec-3a575b3eb12c" src="https://stats.dragoncloud.win/umami.js"></script>
 	<?php
     } 
 }
